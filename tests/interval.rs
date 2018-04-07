f5cf4822acc46aaeb9037e1936ba67147f0b0886
Sam Rijs <srijs@airpost.net>
2018-04-07 11:22:49 +1000

    Use futures 0.2 release

--- tests/interval.rs
+++ tests/interval.rs
@@ -12,7 +12,7 @@ fn single() {
     let dur = Duration::from_millis(10);
     let start = Instant::now();
     let interval = Interval::new(dur);
-    block_on(interval.take(1).collect()).unwrap();
+    block_on(interval.take(1).collect::<Vec<_>>()).unwrap();
     assert!(start.elapsed() >= dur);
 }
 
@@ -21,7 +21,7 @@ fn two_times() {
     let dur = Duration::from_millis(10);
     let start = Instant::now();
     let interval = Interval::new(dur);
-    let result = block_on(interval.take(2).collect()).unwrap();
+    let result = block_on(interval.take(2).collect::<Vec<_>>()).unwrap();
     assert!(start.elapsed() >= dur*2);
     assert_eq!(result, vec![(), ()]);
 }
