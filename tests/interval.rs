9e512f78addc910b60d8a0c942e706c665b76208
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-05-18 12:13:57 +0200

    interval tests done!
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- tests/interval.rs
+++ tests/interval.rs
@@ -1,26 +1,28 @@
-// extern crate futures;
-// extern crate futures_timer;
+#![feature(async_await)]
 
-// use std::time::{Instant, Duration};
+use std::time::{Duration, Instant};
+use std::error::Error;
 
-// use futures::prelude::*;
-// use futures_timer::Interval;
+use futures::prelude::*;
+use futures_timer::Interval;
 
-// #[test]
-// fn single() {
-//     let dur = Duration::from_millis(10);
-//     let start = Instant::now();
-//     let interval = Interval::new(dur);
-//     interval.take(1).collect().wait().unwrap();
-//     assert!(start.elapsed() >= dur);
-// }
+#[runtime::test]
+async fn single() -> Result<(), Box<dyn Error + Send + Sync + 'static>>{
+    let dur = Duration::from_millis(10);
+    let start = Instant::now();
+    let interval = Interval::new(dur);
+    interval.take(1).collect::<Vec<()>>().await;
+    assert!(start.elapsed() >= dur);
+    Ok(())
+}
 
-// #[test]
-// fn two_times() {
-//     let dur = Duration::from_millis(10);
-//     let start = Instant::now();
-//     let interval = Interval::new(dur);
-//     let result = interval.take(2).collect().wait().unwrap();
-//     assert!(start.elapsed() >= dur*2);
-//     assert_eq!(result, vec![(), ()]);
-// }
+#[runtime::test]
+async fn two_times() -> Result<(), Box<dyn Error + Send + Sync + 'static>>{
+    let dur = Duration::from_millis(10);
+    let start = Instant::now();
+    let interval = Interval::new(dur);
+    let result = interval.take(2).collect::<Vec<()>>().await;
+    assert!(start.elapsed() >= dur * 2);
+    assert_eq!(result, vec![(), ()]);
+    Ok(())
+}
