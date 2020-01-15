5ced38b4c6441ea6ef399dc0b5c65a99a37e0808
Ashley <ashley.ruglys@gmail.com>
2020-01-15 11:33:48 +0100

    Add send_wrapper

--- src/delay_wasm.rs
+++ src/delay_wasm.rs
@@ -1,17 +1,20 @@
 //! A version of `Delay` that works on wasm.
 
 use gloo_timers::future::TimeoutFuture;
+use send_wrapper::SendWrapper;
 use std::{time::Duration, pin::Pin, task::{Context, Poll}, future::Future};
 
 /// A version of `Delay` that works on wasm.
 #[derive(Debug)]
-pub struct Delay(TimeoutFuture);
+pub struct Delay(SendWrapper<TimeoutFuture>);
 
 impl Delay {
 	/// Creates a new future which will fire at `dur` time into the future.
 	#[inline]
 	pub fn new(dur: Duration) -> Delay {
-		Self(TimeoutFuture::new(dur.as_millis() as u32))
+		Self(
+			SendWrapper::new(TimeoutFuture::new(dur.as_millis() as u32))
+		)
 	}
 }
 
@@ -19,6 +22,6 @@ impl Future for Delay {
 	type Output = ();
 
 	fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
-		Pin::new(&mut Pin::into_inner(self).0).poll(cx)
+		Pin::new(&mut *Pin::into_inner(self).0).poll(cx)
 	}
 }
