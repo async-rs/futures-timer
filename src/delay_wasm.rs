22505772fd6a3434900ee28cc61f369df4486023
Ashley <ashley.ruglys@gmail.com>
2020-01-15 11:00:14 +0100

    Fix pinning

--- src/delay_wasm.rs
+++ src/delay_wasm.rs
@@ -3,10 +3,12 @@
 use gloo_timers::future::TimeoutFuture;
 use std::{time::Duration, pin::Pin, task::{Context, Poll}, future::Future};
 
+/// A version of `Delay` that works on wasm.
 #[derive(Debug)]
 pub struct Delay(TimeoutFuture);
 
 impl Delay {
+	/// Creates a new future which will fire at `dur` time into the future.
 	#[inline]
 	pub fn new(dur: Duration) -> Delay {
 		Self(TimeoutFuture::new(dur.as_millis() as u32))
@@ -17,6 +19,6 @@ impl Future for Delay {
 	type Output = ();
 
 	fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
-		Pin::new(*self.0).poll(cx)
+		Pin::new(&mut Pin::into_inner(self).0).poll(cx)
 	}
 }
