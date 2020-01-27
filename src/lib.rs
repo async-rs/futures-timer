aa7d2bcd2d3936236e96ac3d94de139b1f1ca898
Yoshua Wuyts <yoshuawuyts@gmail.com>
2020-01-27 12:36:40 +0100

    change main doctest example
    This should make the main doctest easier to follow

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,16 @@
 //!
 //! # Examples
 //!
-//! ```
+//! ```no_run
+//! # #[async_std::main]
+//! # async fn main() {
 //! use std::time::Duration;
 //! use futures_timer::Delay;
 //! use futures::executor::block_on;
 //!
-//! let now = block_on(Delay::new(Duration::from_secs(3)));
+//! let now = Delay::new(Duration::from_secs(3)).await;
 //! println!("waited for 3 secs");
+//! # }
 //! ```
 
 #![deny(missing_docs)]
