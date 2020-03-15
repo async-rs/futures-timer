9f49538b84bb356566b4914b89e807943647a7dc
Ben <benphawke@gmail.com>
2020-03-15 23:21:53 +0100

    Rename files from heap to bin_heap
    The use of 'heap' for labelling the data structure clashes with dynamic
    memory 'heap'. This is purely a naming convention change, and should have
    no affect on implementation.

--- src/native.rs
+++ src/native.rs
@@ -2,14 +2,14 @@ mod arc_list;
 mod atomic_waker;
 mod delay;
 mod global;
-mod heap;
-mod heap_timer;
+mod bin_heap;
+mod bin_heap_timer;
 mod timer;
 
 use self::arc_list::{ArcList, Node};
 use self::atomic_waker::AtomicWaker;
-use self::heap::{Heap, Slot};
-use self::heap_timer::HeapTimer;
+use self::bin_heap::{Heap, Slot};
+use self::bin_heap_timer::HeapTimer;
 use self::timer::{ScheduledTimer, Timer, TimerHandle};
 
 pub use self::delay::Delay;
